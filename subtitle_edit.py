74b95b60b046e67d1b8f378fa7e85bc98b9cf4e7
byung-u <iam.byungwoo@gmail.com>
2017-01-06 19:11:40 +0900

    add subtitle eidt test version

---
+++ subtitle_edit.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.5
+import re
+import sys
+from time import gmtime, strftime
+
+def remove_time(file_name, out_file_name, p3):
+    fw = open(out_file_name, 'w')
+    with open(file_name) as f:
+        for line in f:
+            m = p3.match(line)
+            if m is None:
+                fw.write(line)
+    f.closed
+    fw.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: pyty.py FILE', file=sys.stderr)
+        sys.exit(2)
+
+    file_name = sys.argv[1]
+    out_file_name = '%s.%s' % (file_name, strftime("%Y%m%d%H%M%S", gmtime()))
+
+    # TODO: get option by command line
+
+    # 00:37:24.250--> 00:37:30.160
+    time_record = re.compile(r'\d+\d+:\d+\d+:\d+\d+\.\d+\d+\d+\ -\-\>\ \d+\d+:\d+\d+:\d+\d+\.\d+\d+\d+')
+    remove_time(file_name, out_file_name, time_record)
+
+    sys.exit(0)
